526082d55d1bb0686c847bbcffa4a8f83fc6403d
AxL <rafal.zoltanski@gmail.com>
2018-09-25 21:18:18 +0200

    Test

--- src/main/java/view/ProgramStart.java
+++ src/main/java/view/ProgramStart.java
@@ -1,11 +1,3 @@
-/**
- * <p>PEKAES Tracker v1.0.0 by Rafał (AxL) Żółtański</p>
- *
- * <a href="https://ibb.co/ikY6P9"><img src="https://thumb.ibb.co/ikY6P9/PEKAESTracker_details_data_view.jpg" alt="PEKAESTracker_details_data_view" border="0"></a>
- * <a href="https://ibb.co/ga92xU"><img src="https://thumb.ibb.co/ga92xU/PEKAESTracker_main_data_view.jpg" alt="PEKAESTracker_main_data_view" border="0"></a>
- * */
-
-
 package view;
 
 import controller.InputFormController;
